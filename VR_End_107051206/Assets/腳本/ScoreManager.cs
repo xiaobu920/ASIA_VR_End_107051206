da2a65892df1794483e56c0b4dcda09c3c42940d
xiaobu920 <71679970+xiaobu920@users.noreply.github.com>
2021-01-17 05:49:41 +0800

    音樂跟音效

---
+++ VR_End_107051206/Assets/腳本/ScoreManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("分數介面")]
+    public Text textScore;
+    [Header("分數")]
+    public int score;
+    [Header("預設得分")]
+    public int scoreP = 1;
+    [Header("得分音效")]
+    public AudioClip soundIn;   //存音效
+
+    private AudioSource aud;    //存元件
+
+    private void Awake()
+    {   
+        //音效來源=取得元件<音效來源>()
+        aud = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "可丟物")
+        {
+            AddScore();
+        }
+        if (other.transform.root.name == "Player")
+        {
+            scoreP = 2;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.root.name == "Player")
+        {
+            scoreP = 1;
+        }
+    }
+
+    void AddScore()
+    {
+        score += scoreP;
+        textScore.text = "加料分數：" + score;
+        aud.PlayOneShot(soundIn, Random.Range(0.5f,2f));
+    }
+
+}

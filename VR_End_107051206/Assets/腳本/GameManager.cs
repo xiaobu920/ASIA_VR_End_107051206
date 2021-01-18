458a3dfd893a5f4e28e08ce3ec12134341586136
xiaobu920 <71679970+xiaobu920@users.noreply.github.com>
2021-01-19 02:38:51 +0800

    finish

---
+++ VR_End_107051206/Assets/腳本/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    /// <summary>
+    /// 重新遊戲
+    /// </summary>
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("VR場景");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
